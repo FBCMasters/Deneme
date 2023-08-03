6fafa1dc02ddba00c0f33ef4a817f909430dce37
FBCMasters <141360617+FBCMasters@users.noreply.github.com>
2023-08-03 21:41:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Bu yerin sahibi "FBCMasters"")
+import random
+import time
+Cevaplar = ["Oyun oynamak", "Kedi", "Robloxta bir tycoon oyunu yaptım", "Ev yapımı pizza"]
+while True:
+    print(random.choice(Cevaplar))
+    time.sleep(3)
