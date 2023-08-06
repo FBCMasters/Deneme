48ffadd79ad0cce052f2a6cf98fc1132291a8277
FBCMasters <141360617+FBCMasters@users.noreply.github.com>
2023-08-06 12:37:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import random
-import time
-Cevaplar = ["Oyun oynamak", "Kedi", "Robloxta bir tycoon oyunu yaptım", "Ev yapımı pizza"]
-while True:
-    print(random.choice(Cevaplar))
-    time.sleep(3)
+words = {
+    "FBC",
+    "SUS",
+    "LOL",
+}
+word = input("Anlamadığınız bir kelime yazın: FBC, SUS, LOL (Hepsi büyük harfle)")
+if word == "FBC":
+    print("Ferman Barış Cihangir")
+elif word == "SUS":
+    print("Şüpheli")
+elif word == "LOL":
+    print("Komik bir şeye verilen cevap")
+else:
+    print("Bu kelime listemizde yer almıyor")
